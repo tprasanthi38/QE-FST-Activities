8a57d66fbb836854620610af6f64a52f5a95b6f2
tprasanthi38 <tprasanthi38@gmail.com>
2026-02-03 11:49:59 +0530

    Add files via upload

---
+++ QE-SELENIUM-PYTHON/Activity20.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+import time
+
+driver = webdriver.Firefox()
+driver.get("https://training-support.net/webelements/alerts")
+driver.maximize_window()
+
+print("Page Title:", driver.title)
+
+driver.find_element(By.XPATH, "//*[@id='prompt']").click()
+
+alert = Alert(driver)
+print("Alert Text:", alert.text)
+
+alert.send_keys("awesome")
+alert.accept()
+
+time.sleep(2)
+driver.quit()
